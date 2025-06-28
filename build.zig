d81d5a684f516d7cfb709fa233dc27e472df6efa
e3dio <85405955+e3dio@users.noreply.github.com>
2025-06-27 19:44:53 -0600

    build.zig

--- build.zig
+++ build.zig
@@ -3,10 +3,13 @@ const std = @import("std");
 pub fn build(b: *std.Build) !void {
     const name = "watch";
 
+    const t = b.standardTargetOptions(.{});
+    const o = b.standardOptimizeOption(.{});
+
     const mod = b.createModule(.{
+        .target = t,
+        .optimize = o,
         .root_source_file = b.path(name ++ ".zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
     });
 
     const exe = b.addExecutable(.{
